317c43c781b6dc210768adcab195b8c419868902
eso202 <eslamsaad45@gmail.com>
2022-11-27 00:18:11 +0200

    add toolbar title

--- app/src/main/java/com/app/movieTask/MainActivity.kt
+++ app/src/main/java/com/app/movieTask/MainActivity.kt
@@ -2,6 +2,9 @@ package com.app.movieTask
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.app.movieTask.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -9,8 +12,23 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
   private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+  private val navController by lazy { (supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment).navController }
+  private val appBarConfiguration by lazy {
+    AppBarConfiguration(
+      setOf(
+        R.id.trendingMoviesFragment
+      )
+    )
+  }
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(binding.root)
+    initNavigation()
+  }
+
+  private fun initNavigation() {
+    setSupportActionBar(binding.toolbar)
+    setupActionBarWithNavController(navController, appBarConfiguration)
   }
 }
