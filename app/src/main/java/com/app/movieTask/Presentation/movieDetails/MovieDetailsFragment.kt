0ec28106dbc92be289ff65399b8f4de62257dd55
eso202 <eslamsaad45@gmail.com>
2022-11-26 21:36:38 +0200

    first commit

---
+++ app/src/main/java/com/app/movieTask/Presentation/movieDetails/MovieDetailsFragment.kt
@@ -0,0 +1,7 @@
+package com.app.movieTask.Presentation.movieDetails
+
+import androidx.fragment.app.Fragment
+
+class MovieDetailsFragment : Fragment() {
+
+}
---
+++ app/src/main/java/com/app/movieTask/Presentation/movieList/MoviesListFragment.kt
@@ -0,0 +1,7 @@
+package com.app.movieTask.Presentation.movieList
+
+import androidx.fragment.app.Fragment
+
+class MoviesListFragment : Fragment() {
+
+}
