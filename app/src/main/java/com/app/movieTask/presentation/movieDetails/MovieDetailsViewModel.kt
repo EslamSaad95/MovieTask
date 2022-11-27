84a764d6e12432730fbcb6ea6eca5996e1b70cca
eso202 <eslamsaad45@gmail.com>
2022-11-27 15:04:28 +0200

    set movieDetails ViewModel

---
+++ app/src/main/java/com/app/movieTask/presentation/movieDetails/MovieDetailsViewModel.kt
@@ -0,0 +1,40 @@
+package com.app.movieTask.presentation.movieDetails
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.app.movieTask.domain.common.ApiFailure
+import com.app.movieTask.domain.entity.MovieDetailsEntity
+import com.app.movieTask.domain.usecase.MovieDetailsUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+
+@HiltViewModel
+class MovieDetailsViewModel(private val useCase: MovieDetailsUseCase) : ViewModel() {
+
+  private val _movieDetailsLiveData by lazy { MutableLiveData<MovieDetailsEntity>() }
+  val movieDetailsLiveData get() = _movieDetailsLiveData
+
+  private val _loadingLiveData by lazy { MutableLiveData<Boolean>() }
+  val loadingLiveData get() = _loadingLiveData
+
+  private val _errorLiveData by lazy { MutableLiveData<ApiFailure>() }
+  val errorLiveData get() = _errorLiveData
+
+  fun getMovieDetails(movieId: Int) {
+    _loadingLiveData.value = true
+    viewModelScope.launch {
+
+      useCase.getMovieDetails(movieId).collect { response ->
+        response.value?.let {
+          _loadingLiveData.value = false
+          _movieDetailsLiveData.value = it
+        }
+        response.error?.let {
+          _loadingLiveData.value = false
+          _errorLiveData.value = it
+        }
+      }
+    }
+  }
+}
