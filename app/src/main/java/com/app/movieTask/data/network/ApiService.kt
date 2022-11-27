8fd229c67653123b7e3217f9b8c2dc4bc4da0d6f
eso202 <eslamsaad45@gmail.com>
2022-11-27 14:09:43 +0200

    add movieDetails Dto & getway

--- app/src/main/java/com/app/movieTask/data/network/ApiService.kt
+++ app/src/main/java/com/app/movieTask/data/network/ApiService.kt
@@ -1,11 +1,17 @@
 package com.app.movieTask.data.network
 
+import com.app.movieTask.data.network.dto.MovieDetailsDto
 import com.app.movieTask.data.network.dto.TrendingMoviesDto
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface ApiService {
 
   @GET("discover/movie")
   suspend fun getTrendingMovies(): Response<TrendingMoviesDto>
+
+  @GET("movie/{movieId}")
+  suspend fun getMovieDetails(@Path("movieId")movieId:Int):Response<MovieDetailsDto>
+
 }
